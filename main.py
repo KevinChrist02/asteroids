ca739782bdf3652fbbfa5a045a8ac2d9b486db72
whoiskevin <christkevin50@gmail.com>
2025-06-01 09:22:47 +0200

    addding the time method to limit the fps

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    time = pygame.time.Clock()
+    dt = 0  # delta time, welche die Zeit angibt, seit dem der letzte Frame gerendert wurde
+
+    ## loop for the game surface
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        dt = time.tick(60) / 1000  # von millisekunden auf sekunden konvertieren
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
